a5755afc52c9fe6654df09438587ef58ae7eae90
tippi fifestarr <tippi@boober.org>
2023-03-15 12:00:03 -0500

    added clg for playtest

--- app/person.jsx
+++ app/person.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -8,15 +10,18 @@ export default function Person({ name, known_for, profile_path, imagePath }) {
     <div className="h-20 w-20">
       {/* <h1>{name}</h1> */}
       {/* <p>oh, from {known_for}, right?</p> */}
-      <Link className="relative h-16" href={`/about`}>
+      {/* <Link className="relative h-16" href={`/about`}> */}
+      <div className="relative h-16">
         <Image
           src={image}
           alt={`image of ${name}`}
           width={55}
           height={55}
           className="rounded-full items-center"
+          onClick={() => console.log(name)}
         />
-      </Link>
+      </div>
+      {/* </Link> */}
     </div>
   );
 }
