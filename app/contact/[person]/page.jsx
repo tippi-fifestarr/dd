7165cb5f718f53a3f1650673e75f9ad6a97aeecb
tippi fifestarr <tippi@boober.org>
2023-03-25 16:11:51 -0500

    centered and responsive on contact

--- app/contact/[person]/page.jsx
+++ app/contact/[person]/page.jsx
@@ -10,26 +10,30 @@ export default async function ContactPerson({ params }) {
   const res = await data.json();
   console.log(res);
   return (
-    <div className="p-2">
+    <div className="p-2 content-center">
       <h1 className="text-2xl text-slate-200 m-2 p-2 bg-slate-600 rounded-md">
-        {res.name}...
+        <b>{res.name}...</b>
         {res.also_known_as.map((aka) => {
           return " aka " + aka;
         })}
       </h1>
 
-      <div className="flex flex-row p-2 bg-black bg-opacity-10 rounded-lg">
+      <div className="flex flex-col md:flex-row w-full p-2 bg-black bg-opacity-10 rounded-lg justify-center items-center">
         <Image
           src={imagePath + res.profile_path}
           alt={`image of ${res.name}`}
-          className="object-contain rounded-md p-1"
+          className="object-contain rounded-lg p-1 justify-self-center"
           height={256}
           width={256}
+          priority
         />
         <div className="flex flex-col">
           <div className="text-xl text-slate-300 my-2 p-2">{res.biography}</div>
           <div className="text-xl flex-col text-slate-200 my-2 p-2">
-            birthed: {res.birthday} in the land of {res.place_of_birth}
+            birthed:{" "}
+            <b>
+              {res.birthday} in the land of {res.place_of_birth}{" "}
+            </b>
           </div>
         </div>
       </div>
